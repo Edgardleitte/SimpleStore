108da2de8d2878196aaf2cc3a7dac9605c126c12
Edgard Leitte <82025374+Edgardleitte@users.noreply.github.com>
2025-02-21 11:45:03 +0100

    Create Program.cs

---
+++ Frontend/Program.cs
@@ -0,0 +1,10 @@
+Program.csusing Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
+
+await builder.Build().RunAsync();
